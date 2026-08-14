9e4993cc55560240939d88ac7f088f6e528d8df1
Divya Mali <divyamali4112006@gmail.com>
2026-08-14 07:08:14 +0530

    Create college database and student table

--- basic_sql.sql
+++ basic_sql.sql
@@ -1,1 +1,12 @@
+ -- creating database college
+CREATE DATABASE college;
+USE college;
 
+
+-- creating table student
+
+CREATE TABLE student
+(id INT, name VARCHAR(50), age INT, address VARCHAR(70));
+
+-- to display table
+SELECT *FROM student;
