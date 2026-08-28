2c4e7f0f33c76fdadcb77f0a1d887b082b213726
Divya Mali <divyamali4112006@gmail.com>
2026-08-28 18:52:06 +0530

    Create college database and teacher table with data
    Created a database named 'college' and a table 'teacher' with relevant fields. Inserted sample data into the 'teacher' table.

--- question_in_sql.sql
+++ question_in_sql.sql
@@ -1,2 +1,21 @@
 -- Q1 create database of college and create table teacher and students.
        
+CREATE DATABASE   college;
+
+USE  college;
+
+CREATE TABLE teacher
+(id INT ,
+ NAME VARCHAR(30),
+ subject VARCHAR(40), 
+ salary INT);
+  
+-- insert values in table teacher
+INSERT INTO teacher
+(id,name,subject,salary)
+values
+(23,"ajay","math",50000),
+(47,"BHARAT","english",60000),
+(18,"chetan","chemistry",45000),
+(9,"divya","physics",75000);
+
